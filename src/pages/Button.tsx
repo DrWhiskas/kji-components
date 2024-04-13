12e1cf04385af9e7cb2d1f94f43ed42a9de16461
unknown <kayv@hotmail.fr>
2024-04-13 04:44:54 +0200

    update

--- src/pages/Button.tsx
+++ src/pages/Button.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import BasicButton from '../components/Button/BasicButton';
-import '../styles/pageButton.css'
 
 export default function Button() {
 	return (
